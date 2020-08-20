a982161adc631bd7f4980b485ae89f0baae49644
naerim <naerim1119@gmail.com>
2020-08-20 20:17:39 +0900

    [ADD] Category Model

--- grace/models.py
+++ grace/models.py
@@ -2,12 +2,33 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Recommend(models.Model):
+class Category(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+
+    slug = models.SlugField(unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return '/grace/category/{}/'.format(self.slug)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+
+class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
+
+    head_image = models.ImageField(upload_to='grace/%Y/%m/%d/', blank=True)
+
+    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL)
 
     created = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=True)
 
     def __str__(self):
         return '{} :: {}'.format(self.title, self.author)
+
+
