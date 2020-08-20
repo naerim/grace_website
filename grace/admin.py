7ff62b583a9c93bcca3e3b82dd16b86535e88a50
naerim <naerim1119@gmail.com>
2020-08-20 20:17:19 +0900

    [FIX] admin.py

--- grace/admin.py
+++ grace/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Recommend
+from .models import Post, Category
 
-admin.site.register(Recommend)
+admin.site.register(Post)
+admin.site.register(Category)
